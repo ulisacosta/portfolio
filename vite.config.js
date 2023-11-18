4608f7c689fdc6517df3511421350839bb7d69a4
ulises <ulisacost@gmail.com>
2023-11-18 02:53:13 -0300

    test

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["/src/components/navbar/Navbar"],
+      external: ["../navbar/Navbar"],
     },
   },
 })
