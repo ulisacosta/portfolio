46c803e59b3f67c689e617351ce9a1b2c223d96f
ulises <ulisacost@gmail.com>
2023-11-21 01:54:03 -0300

    test

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,15 +1,15 @@
 import { React } from "react";
 import { dataNavbar, dataNavbarSM } from "./Navbar.data";
 import { Link, useLocation } from "react-router-dom";
-import { motion } from "framer-motion";
-
+/* import { motion } from "framer-motion";
+ */
 export default function Navbar() {
   let location = useLocation();
   return (
     <div className="flex flex-row justify-around items-center w-full  md:fixed md:z-20 md:flex md:flex-col md:items-center md:justify-between md:mt-auto md:h-2/5 md:top-1/4 md:left-10 md:w-16 border-2 border-white rounded-xl  xl:left-auto xl:w-3/6 xl:mt-8 xl:h-16 xl:fixed xl:top-0 xl:z-20 xl:flex xl:flex-row xl:items-center mt-3 mb-3">
       <div className="flex justify-start gap-3 md:flex md:flex-col md:justify-start md:gap-3 md:mt-3 xl:mt-0 xl:w-full xl:flex xl:flex-row xl:justify-start xl:gap-4 xl:ml-2">
         {dataNavbar.map(({ name, path, icon }) => (
-          <motion.div whileTap={{ scale: 0.8 }}>
+
             <a
               href={path}
               className={`${
@@ -24,7 +24,7 @@ export default function Navbar() {
               </div>
               {icon}
             </a>
-          </motion.div>
+          
         ))}
       </div>
 
