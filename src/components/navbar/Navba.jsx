3a11ff8a1e5405775513b5abc9b25ecce3f6b95a
ulises <ulisacost@gmail.com>
2023-11-21 02:08:40 -0300

    test

--- src/components/navbar/Navba.jsx
+++ src/components/navbar/Navba.jsx
@@ -1,5 +1,5 @@
 import { React } from "react";
-import { dataNavbar, dataNavbarSM } from "./Navbar.data";
+/* import { dataNavbar, dataNavbarSM } from "./Navbar.data" */;
 import { Link } from "react-router-dom";
 /* import { motion } from "framer-motion";  */
 
@@ -8,7 +8,7 @@ export default function Navbar() {
   return (
     <div className="flex flex-row justify-around items-center w-full  md:fixed md:z-20 md:flex md:flex-col md:items-center md:justify-between md:mt-auto md:h-2/5 md:top-1/4 md:left-10 md:w-16 border-2 border-white rounded-xl  xl:left-auto xl:w-3/6 xl:mt-8 xl:h-16 xl:fixed xl:top-0 xl:z-20 xl:flex xl:flex-row xl:items-center mt-3 mb-3">
      <div className="flex justify-start gap-3 md:flex md:flex-col md:justify-start md:gap-3 md:mt-3 xl:mt-0 xl:w-full xl:flex xl:flex-row xl:justify-start xl:gap-4 xl:ml-2">
-        {dataNavbar.map(({ name, path, icon }) => (
+   {/*      {dataNavbar.map(({ name, path, icon }) => (
        
             <Link
               to={path}
@@ -23,7 +23,7 @@ export default function Navbar() {
               {icon}
             </Link>
           
-        ))}
+        ))} */}
       </div>
 
       <div className="hidden xl:w-full xl:flex xl:justify-center xl:gap-1 text-white ">
@@ -31,11 +31,11 @@ export default function Navbar() {
         <p className="text-2xl font-semibold ">Ulises</p>
       </div>
       <div className="flex justify-end gap-3 md:flex md:flex-col md:justify-center md:gap-3 md:mb-3 xl:mb-0 xl:w-full xl:flex xl:flex-row xl:justify-end xl:gap-4 xl:mr-2">
-        {dataNavbarSM.map(({ name, link, icon }) => (
+      {/*   {dataNavbarSM.map(({ name, link, icon }) => (
           <a href={link} target="__blank">
             {icon}
           </a>
-        ))}
+        ))} */}
       </div>
     </div>
   );
