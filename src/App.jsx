bab93d3dc081d25b101cb703a250d2e6300e0de6
ulises <ulisacost@gmail.com>
2023-11-25 18:51:04 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,23 @@
-
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Projects from "./components/projects/Projects"
-import Home from "./components/home/Home"
-import About from "./components/about/About"
-import Navbar from "./components/navbar/Navbar"
-
-
+import Projects from "./components/projects/Projects";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <>
-    <div  className="flex justify-center items-center">
-  <Navbar></Navbar>
-    </div>
-    
+      <div className="flex justify-center items-center">
+        <Navbar></Navbar>
+      </div>
 
-  
-        <Routes>
-          <Route  path="/" element={<Home/>} />
-          <Route  path="/home" element={<Home/>} />
-          <Route  path="/about" element={<About  />} />
-          <Route  path="/projects" element={<Projects   />} />
-        </Routes>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
     </>
   );
 }
